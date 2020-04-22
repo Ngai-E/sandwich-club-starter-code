8b420509faeabbc9d11d79b69e77e5a55464dc59
Elizabeth <asobingai@gmail.com>
2020-04-22 18:51:19 +0100

    add json parsing to sandwhich club app

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -21,6 +24,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +47,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +60,62 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView mAlsoKnowAs = findViewById(R.id.tv_detail_also_known_as_label);
+        TextView mPlaceOfOrigin = findViewById(R.id.tv_detail_place_of_origin);
+        TextView mIngredients = findViewById(R.id.tv_detail_ingredients);
+        TextView mDescription = findViewById(R.id.tv_description);
 
+        //show also know as name
+        showAlsoKnownAs(sandwich, mAlsoKnowAs);
+
+        //add ingredients to the view
+        showIngredients(sandwich, mIngredients);
+
+        //add place of origin
+        showOrigin(sandwich, mPlaceOfOrigin);
+
+        //add description
+        showDescription(sandwich, mDescription);
+    }
+
+    private  void showAlsoKnownAs(Sandwich sandwich, TextView mAlsoKnowAs ) {
+        if (sandwich.getAlsoKnownAs() != null && sandwich.getAlsoKnownAs().size() > 0) {
+            for (String name : sandwich.getAlsoKnownAs()) {
+                mAlsoKnowAs.append(name);
+                if (!sandwich.getAlsoKnownAs().get(sandwich.getAlsoKnownAs().size() -1).equals(name)) {
+                    mAlsoKnowAs.append(", ");
+                }
+            }
+        } else {
+            mAlsoKnowAs.setText(R.string.no_answer);
+        }
+    }
+
+    private  void showIngredients(Sandwich sandwich, TextView mIngredients) {
+        if (sandwich.getIngredients() != null && sandwich.getIngredients().size() > 0) {
+            for (String ingredient: sandwich.getIngredients()) {
+                mIngredients.append(" - " + ingredient + "\n");
+            }
+        } else {
+            mIngredients.setText(R.string.no_answer);
+        }
+
+    }
+
+    private  void showOrigin(Sandwich sandwich, TextView mPlaceOfOrigin) {
+        if (sandwich.getPlaceOfOrigin() != null && !sandwich.getPlaceOfOrigin().equals("")) {
+            mPlaceOfOrigin.setText(sandwich.getPlaceOfOrigin());
+        } else {
+            mPlaceOfOrigin.setText(R.string.no_answer);
+        }
+    }
+
+    private  void showDescription(Sandwich sandwich, TextView mDescription) {
+        if (sandwich.getDescription() != null && !sandwich.getDescription().equals("")) {
+            mDescription.setText(sandwich.getDescription());
+        } else {
+            mDescription.setText(R.string.no_answer);
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,64 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+        try {
+            sandwich = new Sandwich();
+            JSONObject jsonObject = new JSONObject(json);
+            if(jsonObject.has("name")) {
+                JSONObject name = jsonObject.getJSONObject("name");
+
+                sandwich.setMainName(name.has("mainName") ?  name.getString("mainName") : "");
+
+                List<String> lisOfOthernames = new ArrayList<>();
+
+                JSONArray jsonArray = name.getJSONArray("alsoKnownAs");
+
+                if (jsonArray != null) {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        lisOfOthernames.add(jsonArray.getString(i));
+                    }
+                    sandwich.setAlsoKnownAs(lisOfOthernames);
+                }
+            }
+            if (jsonObject.has("ingredients")) {
+                List<String> lisOfIngredients = new ArrayList<>();
+
+                JSONArray jsonArray = jsonObject.getJSONArray("ingredients");
+
+                if (jsonArray != null) {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        lisOfIngredients.add(jsonArray.getString(i));
+                    }
+                    sandwich.setIngredients( lisOfIngredients);
+                }
+            }
+            if (jsonObject.has("description")) {
+                sandwich.setDescription(jsonObject.getString("description"));
+            }
+            if (jsonObject.has("image")) {
+                sandwich.setImage(jsonObject.getString("image"));
+            }
+            if (jsonObject.has("placeOfOrigin")) {
+                sandwich.setPlaceOfOrigin(jsonObject.getString("placeOfOrigin"));
+            }
+        } catch (JSONException ex) {
+            Log.e("Error ******", ex.getMessage());
+            ex.printStackTrace();
+        }
+        return sandwich;
     }
 }
